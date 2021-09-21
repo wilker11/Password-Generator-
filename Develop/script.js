f847d9dcb1b3307f0b40a6d72cdc4ff2e6b79d02
Wilker Lino <wilkerlino1@gmail.com>
2021-09-21 12:43:33 +0930

    added the upper case fucntion

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,20 @@ var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
-// Start Password variable values: 
+
 // Special characters 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
 // Numeric characters
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 // Alphabetical characters
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+space = [];
+
+var choices;
+// this is the Upper Case function 
+var toUpper = function (x) {
+    return x.toUpperCase();
+};
