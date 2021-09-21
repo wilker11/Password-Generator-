dba8340b15f15864af50f508ab786bc65b03f856
Wilker Lino <wilkerlino1@gmail.com>
2021-09-21 12:24:46 +0930

    added commenting to the file

--- Develop/script.js
+++ Develop/script.js
@@ -21,5 +21,7 @@ function generatePassword() {
   for (var i = 0, n = charset.length; i < length; ++i) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
   }
-  return retVal;
+  return retVal; 
 }
+
+
